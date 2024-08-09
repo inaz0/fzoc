12c93dad1968e222ae57e86badab899154bf2ee6
inaz0 <inazo.wh@gmail.com>
2024-08-09 09:41:40 +0200

    delta init

--- config_example.php
+++ config_example.php
@@ -7,4 +7,7 @@ $bdd_name     = 'fzco';
 
 $is_maintenace_mode = false;
 
+//-- ufbt configuration
+$path_to_ufbt = __DIR__.'/ufbt/';
+
 ?>
---
+++ delta.sql
@@ -0,0 +1,54 @@
+# File for update the database, on the first version is init File
+
+INSERT INTO `fzco_firmware` VALUES 
+(1,
+'Official',
+'https://update.flipperzero.one/firmware/directory.json',
+'https://github.com/flipperdevices/flipperzero-firmware',
+1,
+'official');
+
+INSERT INTO `fzco_firmware` VALUES 
+(2,
+'Momentum',
+'https://up.momentum-fw.dev/firmware/directory.json',
+'https://github.com/Next-Flip/Momentum-Firmware',
+1,
+'momentum');
+
+INSERT INTO `fzco_firmware_version` VALUES (
+    1,
+	'2024-07-24 09:59:00',
+	'release',
+	'0.104.0',
+	1
+);
+
+INSERT INTO `fzco_firmware_version` VALUES (
+    2,
+	'2024-07-24 09:59:00',
+	'dev',
+	'dev-ofw',
+	1
+);
+
+INSERT INTO `fzco_firmware_version` VALUES (
+    3,
+	'2024-07-29 02:59:00',
+	'release',
+	'mntm-005',
+	1
+);
+
+INSERT INTO `fzco_firmware_version` VALUES (
+    4,
+	'2024-07-29 02:59:00',
+	'dev',
+	'mntm-dev',
+	1
+);
+
+INSERT INTO `fzco_depend` VALUES (1,1);
+INSERT INTO `fzco_depend` VALUES (1,2);
+INSERT INTO `fzco_depend` VALUES (2,3);
+INSERT INTO `fzco_depend` VALUES (2,4);
