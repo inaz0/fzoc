4b6dbb0dfedf59242b230a3462687934623b3da8
inaz0 <inazo.wh@gmail.com>
2024-08-09 13:59:29 +0200

    adding debug variable

--- config_example.php
+++ config_example.php
@@ -9,6 +9,9 @@ $bdd_port     = 3306;
 
 $is_maintenace_mode = false;
 
+//-- put to false for production
+$debug              = false;
+
 //-- ufbt configuration
 $path_to_ufbt = __DIR__.'/ufbt/';
 
