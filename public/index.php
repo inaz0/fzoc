55c6318fc411b1131dfee1bacacd6e1e2969366f
inaz0 <inazo.wh@gmail.com>
2024-08-13 14:01:35 +0200

    preg

--- public/index.php
+++ public/index.php
@@ -172,8 +172,8 @@ if( $form_is_valid === true ){
     //-- 1 récupérer le .fam et le contrôler
     $curl = curl_init();
 
-    $array_url_base = ['https://github.com', '.git'];
-    $array_url_raw  = ['https://raw.githubusercontent.com', '/-/raw/main/application.fam'];
+    $array_url_base = ['/(https:\/\/github\.com)/iu', '/(https:\/\/gitlab\.com\/.*)(\.git)/'];
+    $array_url_raw  = ['https://raw.githubusercontent.com', '$1/-/raw/main/application.fam'];
     $raw_url = preg_replace( $array_url_base, $array_url_raw, $_POST['git_url'], 1);
 
     // fixe l'URL et les autres options appropriées
