799ac9ec4b6bd5f65a648ecdbcc09fb01ac2814b
inaz0 <inazo.wh@gmail.com>
2024-08-14 09:23:24 +0200

    regex

--- public/index.php
+++ public/index.php
@@ -198,7 +198,6 @@ if( $form_is_valid === true ){
         if( preg_match_all( '/(\s*)?(App\()\s*(appid=")([a-z0-9_-]*)(",)\s*(name=")(.*)\s*(apptype=)(.*)\s*(entry_point=")(.*)[.\s]*/mi', $response_curl, $matches )){
 
             var_dump($matches);
-
         }
     }
 
