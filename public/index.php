c9b9b1c66aaf90f157c5d5a7fa3adf0a53d0a15e
inaz0 <inazo.wh@gmail.com>
2024-08-14 13:43:42 +0200

    regex

--- public/index.php
+++ public/index.php
@@ -195,7 +195,7 @@ if( $form_is_valid === true ){
     if( $response_code_curl === 200 ){
 
         //-- on va checker la structure du fam
-        if( preg_match_all( '/(\s*)?(App\()\s*(appid=")([a-z0-9_-]*)(",)\s*(name=")(.*)\s*(apptype=)(.*)\s*(entry_point=")(.*)[.\s]*/mi', $response_curl, $matches )){
+        if( preg_match_all( '/(\s*)?(App\()\s*(appid=")([a-z0-9_-]*)(",)\s*(name=")(.*)(",)(.*)\s*(apptype=)(.*)\s*(entry_point=")(.*)[.\s]*/mi', $response_curl, $matches )){
 
             $the_app_id   = '';
             $the_app_name = '';
