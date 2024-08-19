4c6b06c2147b4466544ade5367eeee560c4810ae
inaz0 <inazo.wh@gmail.com>
2024-08-19 11:13:30 +0200

    task runner

--- task_runner.php
+++ task_runner.php
@@ -9,9 +9,7 @@ unset( $pending_task[0], $pending_task[1]);
 
 foreach( $pending_task ?? [] as $task_waiting ){
 
-    $task_waiting = $task_list.'/'.$task_waiting;
-
-    rename( $task_waiting, 'running/'.$task_waiting.'.sh' );
+    rename( $task_list.$task_waiting, 'running/'.$task_waiting.'.sh' );
     chmod('running/'.$task_waiting.'.sh', 755);
     shell_exec( './running/'.$task_waiting.'.sh' );
 }
