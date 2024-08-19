7ee07bac1683e17a9641a80388d53078ba9f91d3
inaz0 <inazo.wh@gmail.com>
2024-08-19 11:40:23 +0200

    task

--- task_runner.php
+++ task_runner.php
@@ -13,7 +13,10 @@ foreach( $pending_task ?? [] as $task_waiting ){
 
         if( rename( $task_list.$task_waiting, $task_list.'running/'.$task_waiting ) ) {
 
-            $output = shell_exec( 'bash '.$task_list.'running/'.$task_waiting );
+            ob_start();
+            passthru( 'bash '.$task_list.'running/'.$task_waiting  );
+            $var = ob_get_contents();
+            ob_end_clean(); 
         }
         else{
 
