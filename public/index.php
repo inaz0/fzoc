a8a13f6293040f03204e535da76a9be236ea82ca
inaz0 <inazo.wh@gmail.com>
2024-08-19 16:21:36 +0200

    jquery

--- public/index.php
+++ public/index.php
@@ -357,7 +357,8 @@ if( $form_is_valid === true ){
   <link rel="stylesheet" href="assets/css/skeleton.css">
   <link rel="stylesheet" href="assets/css/custom.css">
   <link rel="stylesheet" href="assets/css/dataTables.dataTables.min.css">
-
+  
+  <script type="text/javascript" src="assets/js/jquery-3.7.1.min.js"></script>
   <script type="text/javascript" src="assets/js/dataTables.min.js"></script>
 
   <link rel="icon" type="image/png" href="../../dist/images/favicon.png">
