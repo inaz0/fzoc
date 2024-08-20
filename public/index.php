b5c6cdd7e80042220ede51b4d802fe4fa177b1ed
inaz0 <inazo.wh@gmail.com>
2024-08-20 09:02:22 +0200

    date

--- public/index.php
+++ public/index.php
@@ -40,7 +40,7 @@ $translation  = [
         ],
         'success'          => 'La compilation va bientôt commencer rafraichissez la page régulièrement pour voir le résultat apparaitre ci-dessous.',
         'title_table_app'  => 'Applications compilées',
-        'date_format'      => 'd-m-Y H:i:s',
+        'date_format'      => 'd/m/Y H\hi',
 
     ],
     'en' => 
