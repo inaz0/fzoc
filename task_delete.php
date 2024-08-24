4baf4a20770b7927fa4b37cc7a4f2ceca7e4f407
inaz0 <inazo.wh@gmail.com>
2024-08-24 16:09:19 +0200

    change delete method

--- task_delete.php
+++ task_delete.php
@@ -17,7 +17,9 @@ if( is_array( $sql_compiled_to_delete_res ) && count( $sql_compiled_to_delete_re
 
         try{
 
-            unlink($fap_path.$a_compil_to_delete[ 'compiled_path_fap' ]);
+
+            array_map('unlink', glob( $fap_path.$a_compil_to_delete[ 'compiled_path_fap' ] .'*.fap'));
+            rmdir($fap_path.$a_compil_to_delete[ 'compiled_path_fap' ]);
 
             $sql_update_compiled_stm = $bdd_connexion->prepare( $sql_update_compiled_req );
             $sql_update_compiled_stm->execute( [ 
