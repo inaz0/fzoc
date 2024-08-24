3ab3162a0e58b62912a49e4c3d63d1ce76e6f7e5
inaz0 <inazo.wh@gmail.com>
2024-08-24 16:13:38 +0200

    change unlink path

--- task_delete.php
+++ task_delete.php
@@ -5,7 +5,7 @@ require_once('config.php');
 
 $sql_update_compiled_req = ' UPDATE fzco_compiled SET compiled_status="deleted" WHERE compiled_firmware_version_id=:firm_version AND compiled_application_id=:app_id AND compiled_date=:comp_date ';
 
-$sql_compiled_to_delete_req = ' SELECT * FROM fzco_compiled WHERE  DATEDIFF(NOW(),compiled_date) > 1 AND compiled_status != "deleted"';
+$sql_compiled_to_delete_req = ' SELECT * FROM fzco_compiled INNER JOIN fzco_application ON fzco_compiled.compiled_application_id = fzco_application.application_id WHERE  DATEDIFF(NOW(),compiled_date) > 1 AND compiled_status != "deleted"';
 $sql_compiled_to_delete_req = $bdd_connexion->prepare( $sql_compiled_to_delete_req );
 $sql_compiled_to_delete_req->execute();
 
@@ -18,8 +18,8 @@ if( is_array( $sql_compiled_to_delete_res ) && count( $sql_compiled_to_delete_re
         try{
 
 
-            var_dump(glob( $fap_path.$a_compil_to_delete[ 'compiled_path_fap' ] .'*.fap'));
-            rmdir($fap_path.$a_compil_to_delete[ 'compiled_path_fap' ]);
+            unlink( $fap_path.$a_compil_to_delete[ 'compiled_path_fap' ].'/'. $a_compil_to_delete[ 'application_appid' ] .'.fap' );
+            rmdir( $fap_path.$a_compil_to_delete[ 'compiled_path_fap' ] );
 
             $sql_update_compiled_stm = $bdd_connexion->prepare( $sql_update_compiled_req );
             $sql_update_compiled_stm->execute( [ 
