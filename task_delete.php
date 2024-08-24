ac02cbceda25f6e4f749f17161a9e4d71359f61d
inaz0 <inazo.wh@gmail.com>
2024-08-24 16:18:09 +0200

    set delete time to 30 days

--- task_delete.php
+++ task_delete.php
@@ -5,7 +5,7 @@ require_once('config.php');
 
 $sql_update_compiled_req = ' UPDATE fzco_compiled SET compiled_status="deleted" WHERE compiled_firmware_version_id=:firm_version AND compiled_application_id=:app_id AND compiled_date=:comp_date ';
 
-$sql_compiled_to_delete_req = ' SELECT * FROM fzco_compiled INNER JOIN fzco_application ON fzco_compiled.compiled_application_id = fzco_application.application_id WHERE  DATEDIFF(NOW(),compiled_date) >= 0 AND compiled_status != "deleted"';
+$sql_compiled_to_delete_req = ' SELECT * FROM fzco_compiled INNER JOIN fzco_application ON fzco_compiled.compiled_application_id = fzco_application.application_id WHERE  DATEDIFF(NOW(),compiled_date) >= 30 AND compiled_status != "deleted"';
 $sql_compiled_to_delete_req = $bdd_connexion->prepare( $sql_compiled_to_delete_req );
 $sql_compiled_to_delete_req->execute();
 
